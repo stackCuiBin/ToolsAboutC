f4d7d6d38db2941056274e8f4978469ba596760a
stackCuibin <bb.cui@foxmail.com>
2020-03-11 23:38:59 +0800

    Linux中进程默认启动过程，SECTIONS MEMORY ENTRY

---
+++ class_08/main.c
@@ -0,0 +1,18 @@
+/*************************************************************************
+ ******* File Name: main.c
+ ******* Author: bb.cui
+ ******* Mail: bb.cui@foxmail.com 
+ ******* Created Time: Wed 11 Mar 2020 11:00:39 PM CST
+ ************************************************************************/
+
+#include <stdio.h>
+
+int temp;
+
+int main(int argc, char* argv[])
+{
+	printf("&temp = %p\n", &temp);
+
+    return 0;
+}
+
